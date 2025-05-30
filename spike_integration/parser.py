6a0ee2a7bc6958df7fcba28fe900c69918f73a85
Sayat <127617769+SayatAbdikul@users.noreply.github.com>
2025-05-30 10:04:19 +0500

    changed the spike run and parser for it

--- spike_integration/parser.py
+++ spike_integration/parser.py
@@ -2,8 +2,8 @@ import pexpect
 
 log_file = open("spike_output.txt", "wb")
 pk = '/opt/homebrew/riscv64-unknown-elf/bin/pk'
-file = '/Users/sayat/Documents/GitHub/tracecomp/riscv/test/tests/bin/riscv-tests/rv64ui-p-and'
-cmd = "spike --instructions=500 --log-commits " + pk +" 2> trace.log"
+file = '/Users/sayat/Documents/GitHub/tracecomp/riscv/test/tests/bin/riscv-tests/rv64ui-p-add'
+cmd = "spike -d --log-commits " + " 2> trace.log" + " " + file
 child = pexpect.spawn("/bin/sh", ["-c", cmd])
 
 child.logfile_read = log_file
@@ -13,20 +13,27 @@ child.expect(pexpect.EOF, timeout=None)
 log_file.close()
 log_contents = ""
 # Read the log file and print its contents
-with open("spike_output.log", "r") as f:
+with open("trace.log", "r") as f:
     log_contents = f.read()
 content = []
+pass_next = 0
 for line in log_contents.splitlines():
-    if "0x000000008000003c" in line: # till the .write_tohost
-        break
+    if "(spike)" in line or ">>>>" in line or "exception" in line or pass_next: # ignore the interactive traces
+        if pass_next:
+            pass_next = 0
+        if ">>>>" in line:
+            pass_next = 1
+        continue
     else:
         content.append(line)
 print(len(content))
 log_data = []
 for line in content:
     line_split = line.split()
+    if "tval" in line:
+        continue  # skip lines with tval
     log = {
-        "core": line_split[1],
+        "core": line_split[1][0],
         "something": line_split[2],
         "pc": line_split[3],
         "instruction": line_split[4][1:-1]
@@ -43,4 +50,5 @@ for line in content:
         log["mem"] = None
     log_data.append(log)
 
-print(log_data)
+print(len(log_data))
+print(log_data[-10])
